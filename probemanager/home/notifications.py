3ffa3e68ff5107e400992c1f1e5be0125ae90312
matthieu <matthieu@treussart.com>
2017-12-17 19:08:38 +0100

    Update notifications

--- probemanager/home/notifications.py
+++ probemanager/home/notifications.py
@@ -36,7 +36,7 @@ def send_notification(title, body, html=False):
         try:
             for user in users:
                 if user.is_superuser:
-                    user.email_user(title, html_message=html_body, from_email=settings.DEFAULT_FROM_EMAIL)
+                    user.email_user(title, html_body, from_email=settings.DEFAULT_FROM_EMAIL)
         except ConnectionRefusedError:
             pass
 
