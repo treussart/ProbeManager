9d0e1e3c203d3ee0924739dd2b5964612f52d136
matthieu <matthieu@treussart.com>
2017-12-18 12:32:24 +0100

    Update configuration

--- probemanager/home/models.py
+++ probemanager/home/models.py
@@ -310,7 +310,7 @@ class Configuration(models.Model):
     """
     # General
     key = models.CharField(max_length=100, unique=True, blank=False, null=False)
-    value = models.CharField(max_length=300, blank=False, null=False)
+    value = models.CharField(max_length=300, blank=True, null=False)
 
     def __str__(self):
         return self.key
