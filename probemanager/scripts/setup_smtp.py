431e3ac12f364f4cc6a2a6ab253e498396bafeb3
matthieu <matthieu@treussart.com>
2017-12-19 06:40:16 +0100

    Fix SMTP settings

--- probemanager/scripts/setup_smtp.py
+++ probemanager/scripts/setup_smtp.py
@@ -13,28 +13,24 @@ TLS = {{ use_tls }}
 
 
 def run(*args):
-    skip = input('Skip SMTP settings ? (Y/n) ')
-    if skip.lower() == 'y' or not skip:
-        sys.exit(0)
-    else:
-        print("Server SMTP :")
-        host = input('host : ')
-        host_user = input('user : ')
-        host_password = getpass('password : ')
-        default_from_email = input('default from email : ')
-        use_tls = input('The SMTP host use TLS ? : (True/False) ')
+    print("Server SMTP :")
+    host = input('host : ')
+    host_user = input('user : ')
+    host_password = getpass('password : ')
+    default_from_email = input('default from email : ')
+    use_tls = input('The SMTP host use TLS ? : (True/False) ')
 
-        t = Template(template_smtp)
-        final = t.render(host=host,
-                         host_user=host_user,
-                         default_from_email=default_from_email,
-                         use_tls=str(use_tls)
-                         )
+    t = Template(template_smtp)
+    final = t.render(host=host,
+                     host_user=host_user,
+                     default_from_email=default_from_email,
+                     use_tls=str(use_tls)
+                     )
 
-        with open(args[0] + 'conf.ini', 'a') as f:
-            f.write(final)
-        f.close()
-        with open(args[0] + 'password_email.txt', 'w') as f:
-            f.write(encrypt(host_password).decode('utf-8'))
-        f.close()
-        sys.exit(0)
+    with open(args[0] + 'conf.ini', 'a') as f:
+        f.write(final)
+    f.close()
+    with open(args[0] + 'password_email.txt', 'w') as f:
+        f.write(encrypt(host_password).decode('utf-8'))
+    f.close()
+    sys.exit(0)
