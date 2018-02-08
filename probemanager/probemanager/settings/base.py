9ddc57394ebdb2dfa6c187229d632136c201e7b3
matthieu <matthieu@treussart.com>
2018-02-08 21:32:05 +0100

    API

--- probemanager/probemanager/settings/base.py
+++ probemanager/probemanager/settings/base.py
@@ -127,15 +127,4 @@ REST_FRAMEWORK = {
     )
 }
 
-SWAGGER_SETTINGS = {
-    'USE_SESSION_AUTH': False,
-    'SECURITY_DEFINITIONS': {
-            'api_key': {
-                'type': 'apiKey',
-                'in': 'header',
-                'name': 'Authorization'
-            }
-        },
-}
-
 FIXTURE_DIRS = [BASE_DIR + '/probemanager/fixtures', ]
