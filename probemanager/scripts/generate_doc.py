f79e7873a59c19fabc8061e452538a3447abcc71
matthieu <matthieu@treussart.com>
2018-03-13 18:00:10 +0100

    update install

--- probemanager/scripts/generate_doc.py
+++ probemanager/scripts/generate_doc.py
@@ -2,7 +2,7 @@ from django.apps.registry import apps
 from jinja2 import Template
 from core.models import Probe
 import os
-from django.conf import settings
+from probemanager.settings.base import ROOT_DIR, BASE_DIR
 
 
 def run(*args):
@@ -23,16 +23,16 @@ def run(*args):
 """
     t = Template(template)
     t_include = Template(template_include)
-    with open(settings.ROOT_DIR + '/docs/modules/index.rst', 'w') as f:
+    with open(ROOT_DIR + '/docs/modules/index.rst', 'w') as f:
         for app in apps.get_app_configs():
             for model in app.get_models():
                 if issubclass(model, Probe):
                     if app.verbose_name != "Core":
-                        path = settings.BASE_DIR + "/" + app.label + "/README.rst"
+                        path = BASE_DIR + "/" + app.label + "/README.rst"
                         if os.path.isfile(path):
                             template_rendered = t.render(module=app.label, name=app.verbose_name)
                             template_include_rendered = t_include.render(module=app.label)
-                            f_include = open(settings.ROOT_DIR + '/docs/modules/' + app.label + '.rst', 'w')
+                            f_include = open(ROOT_DIR + '/docs/modules/' + app.label + '.rst', 'w')
                             f_include.write(template_include_rendered)
                             f_include.close()
                             f.write(template_rendered)
