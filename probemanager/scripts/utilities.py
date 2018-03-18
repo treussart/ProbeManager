f3884b86565cc562bf0c943a87ced67130d8b2ed
matthieu <matthieu@treussart.com>
2018-03-18 20:45:42 +0100

    New CT for travis tests

--- probemanager/scripts/utilities.py
+++ probemanager/scripts/utilities.py
@@ -1,14 +1,20 @@
 # sudo /usr/local/share/ProbeManager/venv/bin/python /usr/local/share/ProbeManager/probemanager/scripts/utilities.py
 # -d /usr/local/share/ProbeManager/
+# venv/bin/python probemanager/scripts/utilities.py -d ~/git/Probemanager/
 import argparse
 import sys
+import os
 from getpass import getpass
 from cryptography.fernet import Fernet
 
 
 def encrypt(plain_text, dest):
-    with open(dest + 'fernet_key.txt') as f:
-        fernet_key_bytes = bytes(f.read().strip(), 'utf-8')
+    if os.path.exists(dest + 'fernet_key.txt'):
+        with open(dest + 'fernet_key.txt') as f:
+            fernet_key_bytes = bytes(f.read().strip(), 'utf-8')
+    else:
+        from django.conf import settings
+        fernet_key_bytes = settings.FERNET_KEY
     fernet_key = Fernet(fernet_key_bytes)
     return fernet_key.encrypt(plain_text.encode('utf-8'))
 
@@ -17,7 +23,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dest", help="destination", default='/usr/local/share/ProbeManager/')
     args = parser.parse_args()
-    password = getpass('Type the password for the database, followed by [ENTER]: ')
+    password = getpass('Type the password, followed by [ENTER]: ')
     password_encrypted = encrypt(password, args.dest)
     print("Password encrypted : " + password_encrypted.decode('utf-8'))
     sys.exit(0)
