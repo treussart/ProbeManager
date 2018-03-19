d34ddb769e8c6410bb5d3108006bf31a1cd879d5
Treussart Matthieu <matthieu@treussart.com>
2018-03-19 09:43:25 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -16,6 +16,8 @@ done
 sourcecoverage="--source=""$sourcecoverage"
 
 if [[ "$TRAVIS" = true ]]; then
+    ls -la
+    pwd
     flake8 $source --config=.flake8
     coverage erase
     coverage run $sourcecoverage probemanager/runtests.py $arg
