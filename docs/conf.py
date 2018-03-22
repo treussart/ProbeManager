7dfcc5d68b07ba4c8b051a671a1dc4199d918177
matthieu <matthieu@treussart.com>
2018-03-22 22:42:07 +0100

    Problem doc install

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,6 @@ import sys
 import re
 import datetime
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'probemanager.settings.dev'
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import django
