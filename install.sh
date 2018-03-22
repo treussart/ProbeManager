7e4cb0c4b704eaca485ed7439a88969024718a39
matthieu <matthieu@treussart.com>
2018-03-22 20:06:29 +0100

    Fix when probe not yet installed

--- install.sh
+++ install.sh
@@ -393,7 +393,7 @@ launch_celery(){
         echo '## Restart Celery ##'
         kill $( cat "$destfull"probemanager/celery.pid)
         pkill -f celery
-        sleep 5
+        sleep 8
         (cd "$destfull"probemanager/ && "$destfull"venv/bin/celery -A probemanager worker -D --pidfile celery.pid -B -l info -f /var/log/probemanager-celery.log --scheduler django_celery_beat.schedulers:DatabaseScheduler)
     fi
 }
