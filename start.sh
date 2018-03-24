47c665f6fd3091bce712c363178d7e4407cc3190
matthieu <matthieu@treussart.com>
2018-03-24 15:42:23 +0100

    test run server

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ if [ -z $1 ] || [[ "$1" = 'dev' ]]; then
     arg="dev"
 elif [[ "$1" = 'prod' ]]; then
     arg=$1
+elif [[ "$1" = 'travis' ]]; then
+    arg=$1
 else
     echo 'Bad argument'
     exit 1
@@ -31,5 +33,5 @@ if [[ "$arg" != 'travis' ]]; then
     venv/bin/python probemanager/manage.py runserver --settings=probemanager.settings.$arg
 else
     (cd probemanager/ && /home/travis/virtualenv/bin/celery -A probemanager worker -D --pidfile celery.pid -B -l debug -f probemanager-celery.log --scheduler django_celery_beat.schedulers:DatabaseScheduler)
-    /home/travis/virtualenv/bin/python probemanager/manage.py runserver --settings=probemanager.settings.$arg
+    /home/travis/virtualenv/bin/python probemanager/manage.py runserver --settings=probemanager.settings.dev
 fi
