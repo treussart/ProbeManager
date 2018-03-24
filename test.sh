95091699352593c39a91d63260450badc846a45c
matthieu <matthieu@treussart.com>
2018-03-24 22:08:28 +0100

    update test

--- test.sh
+++ test.sh
@@ -15,34 +15,24 @@ do
 done
 sourcecoverage="--source=""$sourcecoverage"
 
-if [[ "$TRAVIS" = true ]]; then
-    flake8 $source --config=.flake8
-    coverage erase
-    coverage run $sourcecoverage probemanager/runtests.py $arg
-    coverage report -i --skip-covered
-    if [ -f .coveralls.yml ]; then
-        coveralls
-    fi
-    if [ -f probemanager/probemanager-error.log ]; then
-        echo "#### LOGS ####"
-        cat probemanager/probemanager-error.log
-    fi
-else
+if [[ "$VIRTUAL_ENV" = "" ]]; then
     if [ ! -d venv ]; then
         echo 'Install before testing'
         exit
-    fi
-    if [[ "$VIRTUAL_ENV" = "" ]]; then
+    else
         source venv/bin/activate
-    fi
-    venv/bin/flake8 $source --config=.flake8
-    venv/bin/coverage erase
-    venv/bin/coverage run $sourcecoverage probemanager/runtests.py $arg
-    venv/bin/coverage report -i --skip-covered
-    venv/bin/coverage html
-    if [ -f .coveralls.yml ]; then
-        venv/bin/coveralls
     fi
 fi
 
-
+flake8 $source --config=.flake8
+coverage erase
+coverage run $sourcecoverage probemanager/runtests.py $arg
+coverage report -i --skip-covered
+coverage html
+if [ -f .coveralls.yml ]; then
+    coveralls
+fi
+if [ -f probemanager/probemanager-error.log ]; then
+    echo "#### LOGS ####"
+    cat probemanager/probemanager-error.log
+fi
