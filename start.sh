c3b226fbd76b392bada2775c0f13e2d1f6e3777a
matthieu <matthieu@treussart.com>
2018-03-24 16:14:16 +0100

    test install prod

--- start.sh
+++ start.sh
@@ -4,8 +4,6 @@ if [ -z $1 ] || [[ "$1" = 'dev' ]]; then
     arg="dev"
 elif [[ "$1" = 'prod' ]]; then
     arg=$1
-elif [[ "$1" = 'travis' ]]; then
-    arg=$1
 else
     echo 'Bad argument'
     exit 1
@@ -19,19 +17,15 @@ if [[ "$VIRTUAL_ENV" = "" ]]; then
         source venv/bin/activate
     fi
 fi
-if [[ "$arg" != 'travis' ]]; then
-    if [ ! -f probemanager/celery.pid ]; then
-        (cd probemanager/ && ../venv/bin/celery -A probemanager worker -D --pidfile celery.pid -B -l debug -f probemanager-celery.log --scheduler django_celery_beat.schedulers:DatabaseScheduler)
-    else
-        kill $( cat probemanager/celery.pid)
-        sleep 3
-        pkill -f celery
-        sleep 3
-        (cd probemanager/ && ../venv/bin/celery -A probemanager worker -D --pidfile celery.pid -B -l debug -f probemanager-celery.log --scheduler django_celery_beat.schedulers:DatabaseScheduler)
-    fi
 
-    venv/bin/python probemanager/manage.py runserver --settings=probemanager.settings.$arg
+if [ ! -f probemanager/celery.pid ]; then
+    (cd probemanager/ && ../venv/bin/celery -A probemanager worker -D --pidfile celery.pid -B -l debug -f probemanager-celery.log --scheduler django_celery_beat.schedulers:DatabaseScheduler)
 else
-    (cd probemanager/ && celery -A probemanager worker -D --pidfile celery.pid -B -l debug -f probemanager-celery.log --scheduler django_celery_beat.schedulers:DatabaseScheduler)
-    python probemanager/manage.py runserver --settings=probemanager.settings.dev
+    kill $( cat probemanager/celery.pid)
+    sleep 3
+    pkill -f celery
+    sleep 3
+    (cd probemanager/ && ../venv/bin/celery -A probemanager worker -D --pidfile celery.pid -B -l debug -f probemanager-celery.log --scheduler django_celery_beat.schedulers:DatabaseScheduler)
 fi
+
+venv/bin/python probemanager/manage.py runserver --settings=probemanager.settings.$arg
