dda58a2731434c81a58d1f40e1c4681f81a35401
Treussart Matthieu <matthieu@treussart.com>
2018-04-04 15:33:29 +0200

    Update git.py

--- probemanager/core/git.py
+++ probemanager/core/git.py
@@ -9,12 +9,12 @@ logger = logging.getLogger(__name__)
 def git_tag(git_root):
     command = settings.GIT_BINARY + ' describe --tags --always'
     command_date = settings.GIT_BINARY + ' log -n 1 --date=short --pretty=%ad'
-    p = subprocess.Popen(command, shell=False, cwd=git_root, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+    p = subprocess.Popen(command, shell=True, cwd=git_root, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                          universal_newlines=True)
     out, err = p.communicate()
     if err:  # pragma: no cover
         logger.exception(str(err))
-    p_date = subprocess.Popen(command_date, shell=False, cwd=git_root, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+    p_date = subprocess.Popen(command_date, shell=True, cwd=git_root, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                               universal_newlines=True)
     out_date, err_date = p_date.communicate()
     out_date.replace('-', '.')
