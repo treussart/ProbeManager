e66e8385563152bcf8cf06f8a17c7adb813df12f
Treussart Matthieu <matthieu@treussart.com>
2018-04-04 14:50:49 +0200

    Update reload.js

--- probemanager/core/static/core/js/reload.js
+++ probemanager/core/static/core/js/reload.js
@@ -1,4 +1,4 @@
 
 django.jQuery(document).ready(function(){
-    django.jQuery('.object-tools').after('<br/><button onclick="window.location.reload(true);">Refresh</button>');
+    django.jQuery('.object-tools').after("<br/><button onclick='window.location.reload(true);'>Refresh</button>");
 });
