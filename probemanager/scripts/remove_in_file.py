2973b6d82ab36e3cd0db183ea7f7700438dc5115
matthieu <matthieu@treussart.com>
2018-04-05 07:43:11 +0200

    TEST coverage with codacy - 2

--- probemanager/scripts/remove_in_file.py
+++ probemanager/scripts/remove_in_file.py
@@ -4,11 +4,12 @@ import re
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--pattern", help="pattern to remove")
+    parser.add_argument("-p", "--pattern", help="pattern to remove", default=[], type=str, action='append')
     parser.add_argument("-f", "--file", help="file")
     args = parser.parse_args()
-    with open(args.file, 'r') as f:
-        replaced = re.sub(args.pattern, '', f.read())
-    with open(args.file, 'w') as f:
-        f.write(replaced)
+    for pattern in args.pattern:
+        with open(args.file, 'r') as f:
+            replaced = re.sub(pattern, '', f.read())
+        with open(args.file, 'w') as f:
+            f.write(replaced)
     exit()
