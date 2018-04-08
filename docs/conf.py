596f634be5bf46b1c333a61e29fef5e02b64b29e
matthieu <matthieu@treussart.com>
2018-04-08 23:31:24 +0200

    Add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -107,7 +107,7 @@ html_theme_path = ["_themes", ]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_favicon = 'data/favicon.ico'
 
 # -- Options for HTMLHelp output ------------------------------------------
 
