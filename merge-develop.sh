7f882c4094491f6fbe5dd2ffd7effb2469f13689
matthieu <matthieu@treussart.com>
2018-05-31 21:33:24 +0200

    Update tests

--- merge-develop.sh
+++ merge-develop.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 git submodule foreach git checkout master
-git submodule foreach git pull origin master
 git checkout master
+
+git submodule foreach git pull origin master
 git pull origin master
 
 git submodule foreach git merge develop
 git merge develop
 
 git submodule foreach git checkout develop
-git submodule foreach git pull origin develop
 git checkout develop
+
+git submodule foreach git pull origin develop
 git pull origin develop
