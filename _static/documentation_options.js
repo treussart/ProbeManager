d12f14f533b7a5cb68c93e72ad689cd9c18511ee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-21 14:21:51 +0000

    Deploy treussart/ProbeManager to github.com/treussart/ProbeManager.git:gh-pages

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v1.2.1-102-g1c1b687-2018-07-21',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
